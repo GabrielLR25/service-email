7a19c60700a8cf4f42575138d1d014d289c07c5d
GabrielLR25 <ramosbrasil25@gmail.com>
2023-11-17 01:53:48 -0300

    Testando push

---
+++ src/main/java/com/ms/email/controlleres/EmailController.java
@@ -0,0 +1,31 @@
+package com.ms.email.controlleres;
+
+import com.ms.email.dtos.EmailDto;
+import jakarta.validation.Valid;
+import com.ms.email.models.EmailModel;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.ms.email.services.EmailService;
+
+@RestController
+public class EmailController {
+//ms-email no google
+    @Autowired
+    EmailService emailService;
+
+    @PostMapping("/sending-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto){
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+
+    }
+
+}
